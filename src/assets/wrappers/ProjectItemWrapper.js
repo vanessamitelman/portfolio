783324c47706434cfcd08a526987a677e812a54f
Vanessa Mitelman <vanessamitelman@gmail.com>
2023-02-15 22:14:39 +0200

    Update ProjectItemWrapper.js

--- src/assets/wrappers/ProjectItemWrapper.js
+++ src/assets/wrappers/ProjectItemWrapper.js
@@ -4,6 +4,7 @@ export const ProjectItemWrapper = styled.div`
   box-shadow: 0px 0px 6px 4px #fff;
   padding: 1rem;
   border-radius: 5px;
+  margin: 4rem;
   .image-container {
     img {
       width: 100%;
